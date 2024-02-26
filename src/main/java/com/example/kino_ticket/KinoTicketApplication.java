7692289a0928408bce186e72ab3c962c134ba686
ahmadalok <51458937+ahmadalok@users.noreply.github.com>
2024-02-26 16:00:58 +0100

    first commit

---
+++ src/main/java/com/example/kino_ticket/KinoTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.kino_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinoTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kino_ticket/KinoTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kino_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
